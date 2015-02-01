2bdd5e1784ea1c53b9f5564b884e75df0edbfdb8
Bryant Syme <prefu@hotmail.com>
2015-01-31 16:13:55 -0800

    added prettify error to test adding errors

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
 /* global console */
+(function () {
+	'use strict';
 
-function guardian() {
-	var tests = [];
-	var h = function () {};
+	function guardian() {
+		var tests = [];
+		var h = function () {};
 
-	function result(pass) {
-		return tests.filter(function (i) {
-			return pass === i.result;
-		}).length;
+		function result(pass) {
+			return tests.filter(function (i) {
+				return pass === i.result;
+			}).length;
+		}
+
+		return {
+			hook: function (cb) {
+				h = cb;
+			},
+			assure: function (test) {
+				tests.push({
+					result: test,
+					detail: h.apply(null, arguments)
+				});
+			},
+			report: function () {
+				return {
+					pass: result(true),
+					fail: result(false),
+					detail: tests.reduce(function (t, i) {
+						return i.detail ? t.push(i.detail) && t : t;
+					}, [])
+				};
+			}
+		};
 	}
 
-	return {
-		hook: function (cb) {
-			h = cb;
-		},
-		assure: function (test) {
-			tests.push({
-				result: test,
-				detail: h(test)
-			});
-		},
-		report: function () {
-			return {
-				pass: result(true),
-				fail: result(false),
-				detail: tests.reduce(function (t, i) {
-					return i.detail ? t.push(i.detail) && t : t;
-				}, [])
-			};
+	var prettify = require('prettify-error');
+
+	var guard = guardian();
+	guard.hook(function (result, message) {
+		if (!result) {
+			console.log(result);
+			console.error(prettify(new Error(message), 2));
 		}
-	};
-}
-
-
-var guard = guardian();
-guard.hook(function (result) {
-	//return 
-});
-guard.assure(true);
-guard.assure(false);
-console.log(guard.report());
+	});
+	guard.assure(true);
+	guard.assure(false, 'This should work so do not be the bad.');
+	var r = guard.report();
+	console.log('Results: ', r.pass, 'passed', r.fail, 'failed');
+}());
