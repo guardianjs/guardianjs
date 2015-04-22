55d0811fccc4132a2eb1b440d57bef89f323f646
Bryant Syme <prefu@hotmail.com>
2015-04-21 20:46:06 -0700

    refactor test results to use observatory

--- test.js
+++ test.js
@@ -3,31 +3,47 @@
 'use strict';
 
 var guardian = require('./guardian'),
-	fs = require('fs');
+  fs = require('fs'),
+  cliColor = require('cli-color'),
+  observatory = require('observatory').settings({
+    prefix: cliColor.cyan('[guardian] ')
+  });
 
-function mergeResults(fails, results) {
-	return fails + Object.keys(results)
-		.reduce(function (r, test) {
-			return results[test] ? r : r + "\nFailure: " + test;
-		}, '');
+function logFailures(failed) {
+  return failed.map(function (test) {
+    observatory.add(test).fail('Test failed');
+  });
 }
 
-function failureMessage() {
-	return Array.prototype.slice.call(arguments, 0)
-		.reduce(mergeResults, '');
+function failures(tests) {
+  return Object.keys(tests)
+    .filter(function (key) {
+      return !tests[key];
+    })
+    .map(function (key) {
+      return key;
+    });
 }
 
 (function executeTests() {
-	var start = new Date();
-	fs.readdir('tests', function (err, files) {
-		var tests = files.map(function (file) {
-			return require('./tests/' + file)(guardian);
-		});
+  var start = new Date();
+  fs.readdir('tests', function (err, files) {
 
-		var failures = failureMessage.apply(null, tests);
-		if (failures) throw new Error(failures);
+    files.map(function (file) {
+      var fileTask = observatory.add('Running: ' + file);
+      var tests = require('./tests/' + file)(guardian);
+      var testCount = Object.keys(tests).length;
+      var fails = failures(tests);
+      if (fails.length) {
+        logFailures(fails);
+        fileTask.fail(fails.length + ' of ' + testCount + ' failed');
+      } else {
+        fileTask.done(testCount + ' passed');
+      }
+    });
 
-		var end = (new Date() - start) / 1000;
-		console.log('Tests executed in', end,'seconds');
-	});
+    var end = (new Date() - start) / 1000;
+
+    observatory.add('Tests executed:').done(end + ' seconds');
+  });
 }());
